0303222ec65b27bbcb639e88492baaf3a52ed68a
carmelc <carmelc@wix.com>
2019-11-24 14:56:13 +0200

    update readme and help details

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ A command line script that allows measuring the execution time of a command line
 
 ##### Installation
 1. Clone (or just copy measure.sh)
-2. Install the script ```install ./measure.sh /usr/local/bin/measure-times```
-3. See the manual ```measure-times -h```
-4. Run it ```measure-times -c "npm run lint" -n 2 -r ~/tmp/ -b "git checkout commit-before-change" -a "git checkout master" -l 7```
- - Mac users - better run with caffeinate: ```caffeinate -i measure-times -c "npm run lint" -n 2 -r ~/tmp/ -b "git checkout commit-before-change" -a "git checkout master" -l 7``` to avoid machine going into idle
+2. Install the script ```install ./measure.sh /usr/local/bin/measure-script-time```
+3. See the manual ```measure-script-time -h```
+4. Run it ```measure-script-time -c "npm run lint" -n 2 -r ~/tmp/ -b "git checkout commit-before-change" -a "git checkout master" -l 7```
+ - Mac users - better run with caffeinate: ```caffeinate -i measure-script-time -c "npm run lint" -n 2 -r ~/tmp/ -b "git checkout commit-before-change" -a "git checkout master" -l 7``` to avoid machine going into idle
 
--- measure.sh
+++ measure.sh
@@ -7,19 +7,51 @@ INIT_BEFORE=""
 INIT_AFTER="git checkout master"
 NUMBER_OF_RUNS=5
 NUMBER_OF_LINES_TO_TAIL=10
+
 SHOULD_RUN='true'
 SHOULD_RUN_BEFORE='true'
 
 GREEN='\033[0;32m'
 WARNING_COLOR='\033[1;33m'
+BOLD='\033[1m'
 NC='\033[0m'
 
 PRINT_HELP () {
-    printf "\
-usage measure [-c] <command (npm t)> [-p <path to results (~/temp/measure-script-time/results)>] [-b <before state>] [-a <after state>] [-n <number of runs (5)>] [-l <number of lines to include in base log>]\
-
-Full Details:
-TBD
+    printf "\n
+${BOLD}NAME${NC}\n
+    measure-script-time -- measure the execution time of a process before and after a change \n
+${BOLD}SYNOPSIS${NC}\n
+    ${BOLD}measure-script-time${NC} [${BOLD}-c${NC} <command (npm t)>] [${BOLD}-p${NC} <path to results (~/temp/measure-script-time/results)>] [${BOLD}-b${NC} <before state>] [${BOLD}-a${NC} <after state>] [${BOLD}-n${NC} <number of runs (5)>] [${BOLD}-l${NC} <number of lines to include in base log>]\n
+${BOLD}DESCRIPTION${NC}\n
+    A command line script that allows measuring the execution time of a command line script\n
+    The command uses ${BOLD}time${NC} command in order to check the performance of the script\n
+    The execution order is as follows:\n
+    do ${BOLD}--number-of-runs${NC} times:\n
+        * Run init before command \n
+        * Run the script \n
+        * Write full log to ${BOLD}--results-path${NC}/logs-full/current-run.before.<index>.txt \n
+        * Write the last ${BOLD}--tail-amount${NC} lines of the log to ${BOLD}--results-path${NC}/results.before.txt \n
+        * Write the output of ${BOLD}time${NC} to ${BOLD}--results-path${NC}/times.txt file \n\n
+        * Run init after command \n
+        * Run the script \n
+        * Write full log to ${BOLD}--results-path${NC}/logs-full/current-run.after.<index>.txt \n
+        * Write the last ${BOLD}--tail-amount${NC} lines of the log to ${BOLD}--results-path${NC}/results.after.txt \n
+        * Write the output of ${BOLD}time${NC} to ${BOLD}--results-path${NC}/times.txt file \n
+\n
+    The options are as follows (the default):
+        ${BOLD}-c | --command${NC}          The command to measure (npm t) \n
+        ${BOLD}-p | --results-path${NC}     The location to write the execution results to - will be created if not exists (~/temp/measure-script-time/results) \n
+        ${BOLD}-b | --init-before${NC}      The command to run in order to initialize the environment before the change to measure - if not set, the measurement will be for the script after the change only \n
+        ${BOLD}-a | --init-after${NC}       The script to run in order to initialize the change to measure (git checkout master) \n
+        ${BOLD}-n | --number-of-runs${NC}   How many times should the script be executed before and after (5) \n
+        ${BOLD}-l | --tail-amount${NC}      How lines from the script execution should be written to ${BOLD}--results-path${NC}/results.(before|after).txt (10) \n
+${BOLD}EXAMPLES${NC}\n
+    The command:\n
+        measure-script-time -c \"npm run test\" -p ~/tmp/results -b \"npm install some-lib@2\" -a \"npm install some-lib@3\" \n
+    Will measure the execution time of 'npm run test' with version 2 compared to 3 of 'some-lib' npm library \n
+    The command:\n
+        measure-script-time -c \"npm run test\" -p ~/tmp/results -a \"\" \n
+    Will measure the execution time of 'npm run test' without any initialization script and will compare it to nothing \n
 "
 }
 
